46ca65830e1faa1b2b8349155524eaef427a4a68
Holly Fox <fox@holly-pc>
2025-05-03 18:34:29 +0100

    chore(landing): add Lighthouse CI config and update .gitignore to exclude temp lighthouse profiles

---
+++ landing/lighthouserc.js
@@ -0,0 +1,23 @@
+/** @type {import('@lhci/cli').Config} */
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://127.0.0.1:3000/'],
+      numberOfRuns: process.env.NUMBER_OF_RUNS || 3,
+      chromePath: process.env.CHROME_PATH || '/snap/bin/chromium',
+      chromeFlags:
+        '--headless=new --no-sandbox --disable-dev-shm-usage --disable-gpu',
+      settings: { preset: 'desktop', throttlingMethod: 'provided' }
+    },
+    assert: {
+      assertions: {
+        'largest-contentful-paint': ['error', { maxNumericValue: 2500 }],
+        'cumulative-layout-shift':  ['warn',  { maxNumericValue: 0.1 }],
+      }
+    },
+    upload: {
+      target: 'filesystem',
+      outputDir: './.lhci-reports'
+    }
+  }
+}; 
