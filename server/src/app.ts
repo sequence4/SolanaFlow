58b9f310d4b90425769690d29e09fabd9f174c48
Holly Fox <fox@holly-pc>
2025-05-09 22:23:44 +0100

    chore(server): added dotenv-flow package for .env.*

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,6 +10,7 @@ import aiRoutes from './routes/aiRoutes';
 import { errorHandler } from './middleware/errorHandler';
 import containerRoutes from './routes/containerRoutes';
 import cookieParser from 'cookie-parser';
+import 'dotenv-flow/config'; 
 
 dotenv.config();
 
