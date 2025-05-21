73924bb2de3040a44d57da565d8203e697b294ab
Holly Fox <fox@holly-pc>
2025-05-21 12:36:20 +0100

    fix(workspace): default USER_WORKSPACE_IMAGE -> solanaflow:latest

--- server/src/utils/projectUtils.ts
+++ server/src/utils/projectUtils.ts
@@ -13,7 +13,7 @@ import {
   serverIndexContent, 
 } from '../data/templateFiles';
 
-const USER_WORKSPACE_IMAGE = "ghcr.io/hhdgknsn/workspace:anchor";
+const USER_WORKSPACE_IMAGE = "solanaflow:latest";
 
 function hasWarning(output: string): boolean {
   const lowercasedOutput = output.toLowerCase();
