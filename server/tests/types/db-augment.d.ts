4d216f387353d13ec32a0226938747df79c30c08
Holly Fox <fox@holly-pc>
2025-05-26 17:20:44 +0100

    test(types): add connect and query methods to Pool type augmentation

--- server/tests/types/db-augment.d.ts
+++ server/tests/types/db-augment.d.ts
@@ -1,5 +1,10 @@
 declare module 'src/config/database' {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  interface Pool { __resetFakeClient: () => void }
+  interface Pool { 
+    __resetFakeClient: () => void;
+    connect: () => Promise<any>;
+    query: jest.Mock;
+  }
   export const __resetFakeClient: () => void;
+  export default Pool;
 } 
