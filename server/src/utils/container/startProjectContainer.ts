24cc2d4306d7f5e8613c89518f0534efd9cf8a13
Holly Fox <fox@holly-pc.localdomain>
2025-06-02 13:17:03 +0100

    perf(cache): mount target volume where Cargo actually writes and copy warm artefacts correctly

--- server/src/utils/container/startProjectContainer.ts
+++ server/src/utils/container/startProjectContainer.ts
@@ -27,6 +27,7 @@ export async function startProjectContainer(projId: string): Promise<string> {
        -v ${vCargo}:/root/.cargo \
        -v ${vSccache}:/opt/sccache \
        -v ${vTargetBuild}:/usr/src/target \
+       -e CARGO_TARGET_DIR=/usr/src/target \
        -p 0.0.0.0::3000 \
        ${image} \
        bash -c "cd /usr/src && tail -f /dev/null"`,
