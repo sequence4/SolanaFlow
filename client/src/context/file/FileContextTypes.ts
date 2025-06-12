c7670eaf015f765abc42ddc72cb5c693ee254032
Holly Fox <fox@holly-pc.localdomain>
2025-06-12 11:00:59 +0100

    fix: main page detects files in array-based fileTree and widen FileContext type

--- client/src/context/file/FileContextTypes.ts
+++ client/src/context/file/FileContextTypes.ts
@@ -3,6 +3,6 @@ import { FileTreeItemType } from "../../interfaces/FileTreeItemType";
 export interface FileContextType {
   selectedFile: FileTreeItemType | null;
   setSelectedFile: (file: FileTreeItemType | null) => void;
-  fileTree: FileTreeItemType | null;
-  setFileTree: (tree: FileTreeItemType | null) => void;
+  fileTree: FileTreeItemType | FileTreeItemType[] | null;
+  setFileTree: (tree: FileTreeItemType | FileTreeItemType[] | null) => void;
 }
