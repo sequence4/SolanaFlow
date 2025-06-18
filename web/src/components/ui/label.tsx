0654b40110110695969b803088e0a69e2d8269ca
Holly Fox <fox@holly-pc.localdomain>
2025-06-18 17:09:45 +0100

    fix: update component import paths to use @/ alias and fix tooltip implementation

--- web/src/components/ui/label.tsx
+++ web/src/components/ui/label.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
-import { cn } from "../../lib/utils";
+import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const labelVariants = cva(
